32224e213caa2c16bb44f31f5c9ffda500e5c0bb
akasaki1211 <aumogras3@yahoo.co.jp>
2022-09-20 02:42:22 +0900

    First release

--- src/colDetectionNode.cpp
+++ src/colDetectionNode.cpp
@@ -1,18 +1,34 @@
 #include "colDetectionNode.h"
 
 #include <maya/MFnNumericAttribute.h>
+#include <maya/MFnCompoundAttribute.h>
 #include <maya/MVector.h>
 
 MTypeId colDetectionNode::s_id(0x7c001);
+
 MObject colDetectionNode::s_p0;
 MObject colDetectionNode::s_p;
 MObject colDetectionNode::s_r;
 MObject colDetectionNode::s_d;
 MObject colDetectionNode::s_iter;
-MObject colDetectionNode::s_sphereCol_p;
-MObject colDetectionNode::s_sphereCol_r;
+
+MObject colDetectionNode::s_sphereCollider; 
+MObject colDetectionNode::s_sphereColPos;
+MObject colDetectionNode::s_sphereColRad;
+
+MObject colDetectionNode::s_capsuleCollider;
+MObject colDetectionNode::s_capsuleColPosA;
+MObject colDetectionNode::s_capsuleColPosB;
+MObject colDetectionNode::s_capsuleColRadA;
+MObject colDetectionNode::s_capsuleColRadB;
+
+MObject colDetectionNode::s_infinitePlaneCollider;
+MObject colDetectionNode::s_infinitePlaneColPos;
+MObject colDetectionNode::s_infinitePlaneColNor;
+
 MObject colDetectionNode::s_enableGroundCol;
 MObject colDetectionNode::s_groundHeight;
+
 MObject colDetectionNode::s_output;
 
 void* colDetectionNode::creator()
@@ -20,13 +36,18 @@ void* colDetectionNode::creator()
     return new colDetectionNode;
 };
 
+colDetectionNode::SchedulingType colDetectionNode::schedulingType() const
+{
+    return kParallel;
+}
+
 MStatus colDetectionNode::initialize()
 {
     MFnNumericAttribute nAttr;
+    MFnCompoundAttribute cmpAttr;
     MObject x, y, z;
 
-    //s_p0 = nAttr.create("parent", "pr", MFnNumericData::k3Double, 0.0);
-    //nAttr.setKeyable(true);
+    // point attributes
     x = nAttr.create("parentX", "prx", MFnNumericData::kDouble, 0.0);
     y = nAttr.create("parentY", "pry", MFnNumericData::kDouble, 0.0);
     z = nAttr.create("parentZ", "prz", MFnNumericData::kDouble, 0.0);
@@ -52,22 +73,83 @@ MStatus colDetectionNode::initialize()
     nAttr.setMin(0);
     nAttr.setMax(10);
 
+    // collider attributes
+    // sphereCollider
     x = nAttr.create("sphereColInputX", "scinx", MFnNumericData::kDouble, 0.0);
     y = nAttr.create("sphereColInputY", "sciny", MFnNumericData::kDouble, 0.0);
     z = nAttr.create("sphereColInputZ", "scinz", MFnNumericData::kDouble, 0.0);
-    s_sphereCol_p = nAttr.create("sphereColInput", "scin", x, y, z);
+    s_sphereColPos = nAttr.create("sphereColInput", "scin", x, y, z);
     nAttr.setKeyable(true);
 
-    s_sphereCol_r = nAttr.create("sphereCol_radius", "scrad", MFnNumericData::kDouble, 0.0);
+    s_sphereColRad = nAttr.create("sphereColRadius", "scrad", MFnNumericData::kDouble, 0.0);
     nAttr.setKeyable(true);
     nAttr.setMin(0);
 
+    s_sphereCollider = cmpAttr.create("sphereCollider", "sc");
+    cmpAttr.setArray(true);
+    cmpAttr.addChild(s_sphereColPos);
+    cmpAttr.addChild(s_sphereColRad);
+    cmpAttr.setReadable(true);
+    cmpAttr.setUsesArrayDataBuilder(true);
+
+    // capsuleCollider
+    x = nAttr.create("capsuleColInputAX", "ccinax", MFnNumericData::kDouble, 0.0);
+    y = nAttr.create("capsuleColInputAY", "ccinay", MFnNumericData::kDouble, 0.0);
+    z = nAttr.create("capsuleColInputAZ", "ccinaz", MFnNumericData::kDouble, 0.0);
+    s_capsuleColPosA = nAttr.create("capsuleColInputA", "ccina", x, y, z);
+    nAttr.setKeyable(true);
+
+    x = nAttr.create("capsuleColInputBX", "ccinbx", MFnNumericData::kDouble, 0.0);
+    y = nAttr.create("capsuleColInputBY", "ccinby", MFnNumericData::kDouble, 0.0);
+    z = nAttr.create("capsuleColInputBZ", "ccinbz", MFnNumericData::kDouble, 0.0);
+    s_capsuleColPosB = nAttr.create("capsuleColInputB", "ccinb", x, y, z);
+    nAttr.setKeyable(true);
+
+    s_capsuleColRadA = nAttr.create("capsuleColRadiusA", "ccrada", MFnNumericData::kDouble, 0.0);
+    nAttr.setKeyable(true);
+    nAttr.setMin(0);
+
+    s_capsuleColRadB = nAttr.create("capsuleColRadiusB", "ccradb", MFnNumericData::kDouble, 0.0);
+    nAttr.setKeyable(true);
+    nAttr.setMin(0);
+
+    s_capsuleCollider = cmpAttr.create("capsuleCollider", "cc");
+    cmpAttr.setArray(true);
+    cmpAttr.addChild(s_capsuleColPosA);
+    cmpAttr.addChild(s_capsuleColPosB);
+    cmpAttr.addChild(s_capsuleColRadA);
+    cmpAttr.addChild(s_capsuleColRadB);
+    cmpAttr.setReadable(true);
+    cmpAttr.setUsesArrayDataBuilder(true);
+
+    // infinitePlaneCollider
+    x = nAttr.create("infinitePlaneColInputX", "pcinx", MFnNumericData::kDouble, 0.0);
+    y = nAttr.create("infinitePlaneColInputY", "pciny", MFnNumericData::kDouble, 0.0);
+    z = nAttr.create("infinitePlaneColInputZ", "pcinz", MFnNumericData::kDouble, 0.0);
+    s_infinitePlaneColPos = nAttr.create("infinitePlaneColInput", "pcin", x, y, z);
+    nAttr.setKeyable(true);
+
+    x = nAttr.create("infinitePlaneColNormalX", "pcnorx", MFnNumericData::kDouble, 0.0);
+    y = nAttr.create("infinitePlaneColNormalY", "pcnory", MFnNumericData::kDouble, 0.0);
+    z = nAttr.create("infinitePlaneColNormalZ", "pcnorz", MFnNumericData::kDouble, 0.0);
+    s_infinitePlaneColNor = nAttr.create("infinitePlaneColNormal", "pcnor", x, y, z);
+    nAttr.setKeyable(true);
+
+    s_infinitePlaneCollider = cmpAttr.create("infinitePlaneCollider", "pc");
+    cmpAttr.setArray(true);
+    cmpAttr.addChild(s_infinitePlaneColPos);
+    cmpAttr.addChild(s_infinitePlaneColNor);
+    cmpAttr.setReadable(true);
+    cmpAttr.setUsesArrayDataBuilder(true);
+    
+    // groundCollider
     s_enableGroundCol = nAttr.create("enableGroundCol", "gCol", MFnNumericData::kBoolean, false);
     nAttr.setKeyable(true);
 
     s_groundHeight = nAttr.create("groundHeight", "gHeight", MFnNumericData::kDouble, 0.0);
     nAttr.setKeyable(true);
 
+    // output attribute
     x = nAttr.create("outputX", "outx", MFnNumericData::kDouble, 0.0);
     y = nAttr.create("outputY", "outy", MFnNumericData::kDouble, 0.0);
     z = nAttr.create("outputZ", "outz", MFnNumericData::kDouble, 0.0);
@@ -80,8 +162,17 @@ MStatus colDetectionNode::initialize()
     addAttribute(s_r);
     addAttribute(s_d);
     addAttribute(s_iter);
-    addAttribute(s_sphereCol_p);
-    addAttribute(s_sphereCol_r);
+    addAttribute(s_sphereColPos);
+    addAttribute(s_sphereColRad);
+    addAttribute(s_sphereCollider);
+    addAttribute(s_capsuleColPosA);
+    addAttribute(s_capsuleColPosB);
+    addAttribute(s_capsuleColRadA);
+    addAttribute(s_capsuleColRadB);
+    addAttribute(s_capsuleCollider);
+    addAttribute(s_infinitePlaneColPos);
+    addAttribute(s_infinitePlaneColNor);
+    addAttribute(s_infinitePlaneCollider);
     addAttribute(s_enableGroundCol);
     addAttribute(s_groundHeight);
     addAttribute(s_output);
@@ -91,8 +182,17 @@ MStatus colDetectionNode::initialize()
     attributeAffects(s_r, s_output);
     attributeAffects(s_d, s_output);
     attributeAffects(s_iter, s_output);
-    attributeAffects(s_sphereCol_p, s_output);
-    attributeAffects(s_sphereCol_r, s_output);
+    attributeAffects(s_sphereColPos, s_output);
+    attributeAffects(s_sphereColRad, s_output);
+    attributeAffects(s_sphereCollider, s_output);
+    attributeAffects(s_capsuleColPosA, s_output);
+    attributeAffects(s_capsuleColPosB, s_output);
+    attributeAffects(s_capsuleColRadA, s_output);
+    attributeAffects(s_capsuleColRadB, s_output);
+    attributeAffects(s_capsuleCollider, s_output);
+    attributeAffects(s_infinitePlaneColPos, s_output);
+    attributeAffects(s_infinitePlaneColNor, s_output);
+    attributeAffects(s_infinitePlaneCollider, s_output);
     attributeAffects(s_enableGroundCol, s_output);
     attributeAffects(s_groundHeight, s_output);
 
@@ -108,34 +208,114 @@ MStatus colDetectionNode::compute(const MPlug& plug, MDataBlock& data)
         MVector p = data.inputValue(s_p).asVector();
         double r = data.inputValue(s_r).asDouble();
         double d = data.inputValue(s_d).asDouble();
-
+        
         long iter = data.inputValue(s_iter).asLong();
 
-        MVector sphereCol_p = data.inputValue(s_sphereCol_p).asVector();
-        double sphereCol_r = data.inputValue(s_sphereCol_r).asDouble();
-
+        MArrayDataHandle sphereColArrayHandle = data.inputArrayValue(s_sphereCollider);
+        unsigned int scCount = sphereColArrayHandle.elementCount();
+        
+        MArrayDataHandle capsuleColArrayHandle = data.inputArrayValue(s_capsuleCollider);
+        unsigned int ccCount = capsuleColArrayHandle.elementCount();
+        
+        MArrayDataHandle iPlaneColArrayHandle = data.inputArrayValue(s_infinitePlaneCollider);
+        unsigned int pcCount = iPlaneColArrayHandle.elementCount();
+        
         bool enableGroundCol = data.inputValue(s_enableGroundCol).asBool();
         double groundHeight = data.inputValue(s_groundHeight).asDouble();
 
-        
         // detection
+        MVector sphereCol_p;
+        double sphereCol_r;
+        MVector capsuleCol_pA;
+        MVector capsuleCol_pB;
+        double capsuleCol_rA;
+        double capsuleCol_rB;
+        MVector iPlaneCol_p;
+        MVector iPlaneCol_n;
+
         for (int i = 0; i < iter; i++)
         {
             //sphere collision
-            if ((p - sphereCol_p).length() < (sphereCol_r + r))
-            {
-                p = sphereCol_p + ((p - sphereCol_p).normal() * (sphereCol_r + r));
-            }
+            for (unsigned int i = 0; i < scCount; i++) {
+                sphereColArrayHandle.jumpToElement(i);
+                MDataHandle sphereCollider = sphereColArrayHandle.inputValue();
+                sphereCol_p = sphereCollider.child(s_sphereColPos).asVector();
+                sphereCol_r = sphereCollider.child(s_sphereColRad).asDouble();
+                
+                if ((p - sphereCol_p).length() < (sphereCol_r + r))
+                {
+                    p = sphereCol_p + ((p - sphereCol_p).normal() * (sphereCol_r + r));
+                };
 
+                //sphereColArrayHandle.next();
+            };
+
+            //capsule collision
+            for (unsigned int i = 0; i < ccCount; i++) {
+                capsuleColArrayHandle.jumpToElement(i);
+                MDataHandle capsuleCollider = capsuleColArrayHandle.inputValue();
+                capsuleCol_pA = capsuleCollider.child(s_capsuleColPosA).asVector();
+                capsuleCol_rA = capsuleCollider.child(s_capsuleColRadA).asDouble();
+                capsuleCol_pB = capsuleCollider.child(s_capsuleColPosB).asVector();
+                capsuleCol_rB = capsuleCollider.child(s_capsuleColRadB).asDouble();
+                
+                double h = (capsuleCol_pB - capsuleCol_pA).length();
+                MVector ab = (capsuleCol_pB - capsuleCol_pA).normal();
+
+                double t = ab * (p - capsuleCol_pA);
+                double ratio = t / h;
+                if (ratio <= 0)
+                {
+                    if ((p - capsuleCol_pA).length() < (capsuleCol_rA + r))
+                    {
+                        p = capsuleCol_pA + ((p - capsuleCol_pA).normal() * (capsuleCol_rA + r));
+                    };
+                }
+                else if (ratio >= 1)
+                {
+                    if ((p - capsuleCol_pB).length() < (capsuleCol_rB + r))
+                    {
+                        p = capsuleCol_pB + ((p - capsuleCol_pB).normal() * (capsuleCol_rB + r));
+                    };
+                }
+                else
+                {
+                    MVector q = capsuleCol_pA + (ab * t);
+                    float rad = capsuleCol_rA * (1.0 - ratio) + capsuleCol_rB * ratio;
+                    if ((p - q).length() < (rad + r))
+                    {
+                        p = q + ((p - q).normal() * (rad + r));
+                    };
+                };
+
+                //capsuleColArrayHandle.next();
+            };
+
+            //infinite plane collision
+            for (unsigned int i = 0; i < pcCount; i++) {
+                iPlaneColArrayHandle.jumpToElement(i);
+                MDataHandle iPlaneCollider = iPlaneColArrayHandle.inputValue();
+                iPlaneCol_p = iPlaneCollider.child(s_infinitePlaneColPos).asVector();
+                iPlaneCol_n = iPlaneCollider.child(s_infinitePlaneColNor).asVector();
+                
+                double distancePointPlane = iPlaneCol_n * (p - iPlaneCol_p);
+                if (distancePointPlane - r < 0)
+                {
+                    p = p - (iPlaneCol_n * (distancePointPlane - r));
+                };
+
+                //iPlaneColArrayHandle.next();
+            };
+            
             //ground collision
             if (enableGroundCol)
             {
                 p[1] = p[1] < (groundHeight + r) ? (groundHeight + r) : p[1];
-            }
+            };
 
             //keep length
             p = p0 + ((p - p0).normal() * d);
-        }
+        };
         
         // set output values
         MDataHandle outputHandle = data.outputValue(s_output);
--- src/colDetectionNode.h
+++ src/colDetectionNode.h
@@ -11,21 +11,33 @@ public:
     MStatus compute(const MPlug& plug, MDataBlock& data) override;
     static void* creator();
     static MStatus initialize();
+    virtual SchedulingType schedulingType() const;
 
     static MTypeId s_id;
 
-    static MObject s_p0;            //parent position vector
-    static MObject s_p;             //input position vector
-    static MObject s_r;             //input position radius
-    static MObject s_d;             //initial length
+    static MObject s_p0;              //parent position vector
+    static MObject s_p;               //input position vector
+    static MObject s_r;               //input position radius
+    static MObject s_d;               //initial length
     
-    static MObject s_iter;          //detection iterations
+    static MObject s_iter;            //iterations of detection
 
-    static MObject s_sphereCol_p;   //sphere collision position
-    static MObject s_sphereCol_r;   //sphere collision radius
+    static MObject s_sphereCollider;  //sphereCollider Array
+    static MObject s_sphereColPos;    //sphereCollider position vector
+    static MObject s_sphereColRad;    //sphereCollider radius
 
-    static MObject s_enableGroundCol;   //calc ground colision
-    static MObject s_groundHeight;  //ground height
+    static MObject s_capsuleCollider;   //capsuleCollider Array
+    static MObject s_capsuleColPosA;    //capsuleCollider position A vector
+    static MObject s_capsuleColPosB;    //capsuleCollider position B vector
+    static MObject s_capsuleColRadA;    //capsuleCollider radius A
+    static MObject s_capsuleColRadB;    //capsuleCollider radius B
+
+    static MObject s_infinitePlaneCollider;  //infinitePlaneCollider Array
+    static MObject s_infinitePlaneColPos;    //infinitePlaneCollider position vector
+    static MObject s_infinitePlaneColNor;    //infinitePlaneCollider world normal
     
-    static MObject s_output;        //output position vector
+    static MObject s_enableGroundCol; //calc ground colision
+    static MObject s_groundHeight;    //ground height
+    
+    static MObject s_output;          //output position vector
 };
